3373bb1eb52892601dc1e6cbc2714e59a37356b0
Eucee <eucee89@gmail.com>
2016-10-29 15:03:55 +0200

    Finit fonction inser_pos

--- main.c
+++ main.c
@@ -20,12 +20,11 @@ struct TypeCell{
     /*TypeChaine80 nom;
     char nom2[80];*/
     int info;
-    TypeCell *suivant;
+    TypeCell *suiv;
 };
 
 void affichier_erreur(){
         printf("Erreur d'allocation memoire");
-        return;
 }
 
 void inserer_en_tete(TypePtrListe *adr_tete, int val)
@@ -36,37 +35,44 @@ void inserer_en_tete(TypePtrListe *adr_tete, int val)
     if (liste == NULL || p_nouv == NULL)
     {
         affichier_erreur();
+        return;
     }
 
     // Insertion de l'element au debut de la liste
     p_nouv = (TypeCell*)malloc(sizeof(TypeCell))
     p_nouv->info = val;
-    p_nouv->suivant = *adr_tete;
+    p_nouv->suiv = *adr_tete;
     *adr_tete = p_nouv;
 }
 
 
 void inserer_pos_k(TypePtrListe *adr_tete, int val, int pos){
-    TypePtrListe p_prec=NULL, p=*adr_tete;
-    TypeListe p_nouv = (TypeCell*)malloc(sizeof(TypeCell));
-    p_nouv->info = val;
+    TypePtrListe rec=NULL, p=*adr_tete;
+    TypePTrListe p_nouv;
     int i=1;
     while(p!=NULL && i<k){
         prec=p;
-        p=p->suivant;
+        p=p->suiv;
         i++;
     }
     if (p==NULL){
         if(k!=i){
-            free(p_nouv);
             affichier_erreur();
-        }
+            return;
+        }//sinon k==i, insertion en debut de liste
+        p_nouv = (TypeCell*)malloc(sizeof(TypeCell));
+        p_nouv->info = val;
         if(k==1){
             inserer_en_tete(adr_tete,val);
+            return;
+        }//sinon k>1, insertion en fin de liste
+        p_nouv->suiv=NULL;
+        p_prec->suiv=p_nouv;
         return;
-        }
-
-    }
+    }//sinon insertion en milieu de liste
+    p_nouv = (TypeCell*)malloc(sizeof(TypeCell));
+    p_nouv->info = val;
+    inserer_en_tete(&prec->suiv,val);
 }
 
 int main()
@@ -78,17 +84,17 @@ int main()
     if (tete==NULL)exit(1);
 
     strcpy(tete->coucou,"Zoé");
-    tete->suivant=NULL;
+    tete->suiv=NULL;
     tete->nombre=0;
 
     free(tete);
 
     /*TypeCell cellule1, cellule2, cellule3;
     strcpy(cellule3.nom,"Zoe");
-    cellule3.suivant=NULL;
+    cellule3.suiv=NULL;
     strcpy(cellule2.nom,"Marie");
-    cellule2.suivant=&cellule3;*/
-    cellule2.suivant=&cellule3;*/
+    cellule2.suiv=&cellule3;*/
+    cellule2.suiv=&cellule3;*/
     return 0;
 }
 
