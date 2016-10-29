0e068a90ab5e437e1dbcc872b27575c79d6d4775
Eucee <eucee89@gmail.com>
2016-10-29 15:58:54 +0200

    Fonction affich +modif mise en forme

--- main.c
+++ main.c
@@ -37,51 +37,70 @@ void afficher_erreur(){
 void inserer_en_tete(TypePtrListe *adr_tete, int val)
 {
     TypePtrListe p_nouv;
-    if (liste == NULL || p_nouv == NULL)
+    if (adr_tete == NULL || p_nouv == NULL)
     {
         afficher_erreur();
         return;
     }
 
     // creation du nouvel element
-    p_nouv = (TypeCell*)malloc(sizeof(TypeCell))
-    p_nouv->info = val;
+    p_nouv = (TypeCell*) malloc(sizeof(TypeCell))
+    p_nouv -> info = val;
     // insertion de l'element au debut de la liste
-    p_nouv->suiv = *adr_tete;
+    p_nouv -> suiv = *adr_tete;
     *adr_tete = p_nouv;
 }
 
+void creation_elem(TypePtrListe *elem, int val){
+    elem = (TypeCell*) malloc (sizeof(TypeCell));
+    elem -> info = val;
+    return elem;
+}
 
 void inserer_pos_k(TypePtrListe *adr_tete, int val, int pos){
-    TypePtrListe rec=NULL, p=*adr_tete;
+    TypePtrListe rec = NULL, p = *adr_tete;
     TypePTrListe p_nouv;
-    int i=1;
-    while(p!=NULL && i<k){
-        prec=p;
-        p=p->suiv;
+    int i = 1;
+    while(p != NULL && i < k){
+        prec = p;
+        p = p -> suiv;
         i++;
     }
-    if (p==NULL){
-        if(k!=i){
+    if (p == NULL){
+        if(k != i){
             afficher_erreur();
             return;
         }//sinon k==i, insertion en debut de liste
-        p_nouv = (TypeCell*)malloc(sizeof(TypeCell));
-        p_nouv->info = val;
-        if(k==1){
+        creation_elem(&p_nouv,val);
+        if(k == 1){
             inserer_en_tete(adr_tete,val);
             return;
         }//sinon k>1, insertion en fin de liste
-        p_nouv->suiv=NULL;
-        p_prec->suiv=p_nouv;
+        p_nouv -> suiv = NULL;
+        p_prec -> suiv = p_nouv;
         return;
     }//sinon insertion en milieu de liste
-    p_nouv = (TypeCell*)malloc(sizeof(TypeCell));
-    p_nouv->info = val;
-    inserer_en_tete(&prec->suiv,val);
+    creation_elem(&p_nouv,val);
+    inserer_en_tete(&prec -> suiv, val);
 }
 
 
+void afficherListe(TypePtrListe *adr_tete)
+{
+    if (adr_tete == NULL)
+    {
+        afficher_erreur();
+        return;
+    }
+    TypePtrListe p = *adr_tete;
+
+    while (p != NULL)
+    {
+        printf("%d -> ", p->info);
+        p = p->suiv;
+    }
+    printf("NULL\n");
+}
 
 /****************************************CORPS****************************************/
 
